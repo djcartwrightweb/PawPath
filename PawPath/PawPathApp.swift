5b54ef8fd7bf193c91763044a1731ab0932f57ff
DJ Cartwright <dj.cartwright.web@gmail.com>
2024-12-03 17:56:44 -0500

    Initial Commit

---
+++ PawPath/PawPathApp.swift
@@ -0,0 +1,17 @@
+//
+//  PawPathApp.swift
+//  PawPath
+//
+//  Created by David Cartwright on 2024-12-03.
+//
+
+import SwiftUI
+
+@main
+struct PawPathApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
