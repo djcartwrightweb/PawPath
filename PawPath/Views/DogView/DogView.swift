2df934b8d4dbaca256a7c2ec7fbc95670585176b
DJ Cartwright <dj.cartwright.web@gmail.com>
2024-12-15 10:13:17 -0500

    [CLEAN] Clean folder structure again and create DogView to show user-created dogs

---
+++ PawPath/Views/DogView/DogView.swift
@@ -0,0 +1,18 @@
+//
+//  DogView.swift
+//  PawPath
+//
+//  Created by David Cartwright on 2024-12-15.
+//
+
+import SwiftUI
+
+struct DogView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DogView()
+}
